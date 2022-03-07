ff2b5b3f74727ffdc0a14ca66892528160fd14b9
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-03-07 18:03:49 +0000

    api for fetching articles by topic

--- src/api.js
+++ src/api.js
@@ -4,10 +4,24 @@ const api = axios.create({
   baseURL: "https://nc-news-cm.herokuapp.com/api",
 });
 
-const fetchArticles = () => {
-  return api.get("/articles").then((res) => {
+const fetchArticles = (topic) => {
+  let query = `/articles`;
+  if (topic) query += `?topic=${topic}`;
+
+  return api.get(query).then((res) => {
     return res.data;
   });
 };
 
-export { fetchArticles };
+const fetchTopics = () => {
+  return api.get("/topics").then((res) => {
+    return res.data;
+  });
+};
+
+const fetchArticlesByTopic = (topic) => {
+  return api.get(`/articles?topic=${topic}`).then((res) => {
+    return res.data;
+  });
+};
+export { fetchArticles, fetchTopics, fetchArticlesByTopic };
