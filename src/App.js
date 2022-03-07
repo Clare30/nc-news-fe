538200f29835af4a4a6f62e82e4496867910b84c
Clare <30181918+Clare30@users.noreply.github.com>
2022-03-07 16:11:32 +0000

    Revert "View all articles"

--- src/App.js
+++ src/App.js
@@ -1,20 +1,7 @@
 import "./App.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ArticleList from "./components/ArticleList";
-import Header from "./components/Header";
-import css from "./App.css";
 
 function App() {
-  return (
-    <BrowserRouter>
-      <div className="App">
-        <Header />
-        <Routes>
-          <Route path="/" element={<ArticleList />}></Route>
-        </Routes>
-      </div>
-    </BrowserRouter>
-  );
+  return <div className="App"></div>;
 }
 
 export default App;
