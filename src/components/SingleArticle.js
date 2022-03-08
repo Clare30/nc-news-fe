3fad92bd3ba90d648959ee1086267dc45f683613
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-03-08 14:48:01 +0000

    component for single article page

---
+++ src/components/SingleArticle.js
@@ -0,0 +1,34 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import * as api from "../api";
+
+export default function SingleArticle() {
+  const [article, setArticle] = useState({});
+  const [isLoading, setIsLoading] = useState(true);
+  const { id } = useParams();
+
+  useEffect(() => {
+    setIsLoading(true);
+    api.fetchArticlesById(id).then(({ article }) => {
+      setArticle(article);
+      setIsLoading(false);
+    });
+  }, [id]);
+  if (isLoading) <p>Loading...</p>;
+  return (
+    <article className="singleArticle">
+      <h1>{article.title}</h1>
+      <dl>
+        <dd>Author: {article.author} </dd>{" "}
+        <dd> {new Date(article.created_at).toLocaleDateString("en-uk")}</dd>
+      </dl>
+      <p>{article.body}</p>
+      <div className="articleButtons">
+        <div className="voteButton">
+          Vote <button>+</button> <button>-</button>
+        </div>
+        <button className="commentButton">Show comments</button>
+      </div>
+    </article>
+  );
+}
