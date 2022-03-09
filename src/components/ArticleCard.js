df0450c4638e07840d84a45a1c3b46308c832f8b
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-03-09 17:16:36 +0000

    remove random speech marks

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -6,7 +6,7 @@ export default function ArticleCard({ article }) {
       <Link to={`/articles/${article.article_id}`}>
         {article.title}
       </Link>
-      <p>By {article.author}</p>{" "}
+      <p>By {article.author}</p>
       <p>{new Date(article.created_at).toLocaleDateString()}</p>
       <p>{article.topic}</p>
     </div>
