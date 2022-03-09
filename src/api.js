d3b77b50a8560596950e3971c00ecfc213c2f851
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-03-09 17:14:21 +0000

    amend route for topics to point to articleslist component

--- src/api.js
+++ src/api.js
@@ -4,8 +4,10 @@ const api = axios.create({
   baseURL: "https://nc-news-cm.herokuapp.com/api",
 });
 
-const fetchArticles = () => {
-  return api.get("/articles").then((res) => {
+const fetchArticles = (topic) => {
+  let request = "/articles";
+  if (topic) request += `?topic=${topic}`;
+  return api.get(request).then((res) => {
     return res.data;
   });
 };
