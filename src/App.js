26cb5f92d92e9b47d2ecd031782abc6484ab79a8
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-03-10 13:03:52 +0000

    add usecontext

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
 import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { useState } from "react";
+import { loggedInUser } from "./components/context";
 import ArticleList from "./components/ArticleList";
 import Header from "./components/Header";
 import NavBar from "./components/NavBar";
 import SingleArticle from "./components/SingleArticle";
+import Users from "./components/Users";
 
 function App() {
+  const [loggedIn, setLoggedIn] = useState("");
   return (
-    <BrowserRouter>
-      <div className="App">
-        <Header />
-        <NavBar />
-        <Routes>
-          <Route path="/" element={<ArticleList />} />
-          <Route path="/:topic" element={<ArticleList />} />
-          <Route path="/articles/:id" element={<SingleArticle />} />
-        </Routes>
-      </div>
-    </BrowserRouter>
+    <loggedInUser.Provider value={{ loggedIn, setLoggedIn }}>
+      <BrowserRouter>
+        <div className="App">
+          <Header />
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<ArticleList />} />
+            <Route path="/:topic" element={<ArticleList />} />
+            <Route path="/articles/:id" element={<SingleArticle />} />
+            <Route path="/login" element={<Users />} />
+          </Routes>
+        </div>
+      </BrowserRouter>
+    </loggedInUser.Provider>
   );
 }
 
