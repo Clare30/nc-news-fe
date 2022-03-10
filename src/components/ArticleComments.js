8bf400efebfdfd04c314f2c7f326604f7a9e9219
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-03-10 13:04:17 +0000

    add comment count

--- src/components/ArticleComments.js
+++ src/components/ArticleComments.js
@@ -2,10 +2,13 @@ import CommentCard from "./CommentCard";
 import Expandable from "./Expandable";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import PostComment from "./PostComment";
 import * as api from "../api";
 
 export default function ArticleComments() {
   const [comments, setComments] = useState([]);
+  const [commentCount, setCommentCount] = useState(0);
+
   const { id } = useParams();
 
   useEffect(() => {
@@ -16,8 +19,12 @@ export default function ArticleComments() {
 
   return (
     <section>
-      Comments {comments.length}{" "}
+      Comments {comments.length + commentCount}
       <Expandable className="commentButton">
+        <PostComment
+          setCommentCount={setCommentCount}
+          commentCount={commentCount}
+        />
         <div className="commentList">
           {comments.map((comment) => {
             return <CommentCard key={comment.comment_id} comment={comment} />;
