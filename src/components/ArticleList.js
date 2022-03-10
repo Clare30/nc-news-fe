81c3c8b4632ada352df272de95cf8de75a1ef0d8
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-03-10 17:55:39 +0000

    amend article list for additional queries

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -2,19 +2,24 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import * as api from "../api";
 import ArticleCard from "./ArticleCard.js";
+import ArticleSort from "./ArticleSort";
 
 export default function ArticleList() {
   const [articles, setArticles] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [sortBy, setSortBy] = useState();
+  const [order, setOrder] = useState();
   const { topic } = useParams();
+
+  console.log(sortBy, order);
 
   useEffect(() => {
     setIsLoading(true);
-    api.fetchArticles(topic).then(({ articles }) => {
+    api.fetchArticles(topic, sortBy, order).then(({ articles }) => {
       setArticles(articles);
       setIsLoading(false);
     });
-  }, [topic]);
+  }, [topic, sortBy, order]);
 
   if (isLoading)
     return (
@@ -26,6 +31,7 @@ export default function ArticleList() {
     );
   return (
     <section className="articleList">
+      <ArticleSort setOrder={setOrder} setSortBy={setSortBy} />
       {articles.map((article) => {
         return <ArticleCard article={article} key={article.article_id} />;
       })}
