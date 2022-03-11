d507a921370c6cfefd5d5217e9657fa49842676f
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-03-11 09:24:55 +0000

    rename / start making toggle

---
+++ src/components/ButtonFunctions.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+export function Expandable({ children }) {
+  const [show, setShow] = useState(false);
+
+  const toggleOpen = () => setShow((currOpen) => !currOpen);
+
+  return (
+    <div>
+      <button className="button is-small is-responsive" onClick={toggleOpen}>
+        {show ? "Hide" : "Show"}
+      </button>
+      {show && children}
+    </div>
+  );
+}
+
+export function Toggle() {}
