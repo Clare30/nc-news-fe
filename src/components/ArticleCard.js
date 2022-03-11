d1c791cba1408afe570192fea89210c7eab04770
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-03-11 15:37:43 +0000

    amend article card

--- src/components/ArticleCard.js
+++ src/components/ArticleCard.js
@@ -2,10 +2,8 @@ import { Link } from "react-router-dom";
 
 export default function ArticleCard({ article }) {
   return (
-    <div className="articleCard" key={article.article_id}>
-      <Link to={`/articles/${article.article_id}`}>
-        {article.title}
-      </Link>
+    <div className="articleCard">
+      <Link to={`/articles/${article.article_id}`}>{article.title}</Link>
       <p>By {article.author}</p>
       <p>{new Date(article.created_at).toLocaleDateString()}</p>
       <p>{article.topic}</p>
