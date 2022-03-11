e607559edd8a7d3022f7be79a22830f0015d4764
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-03-11 09:24:04 +0000

    refactor exports

--- src/api.js
+++ src/api.js
@@ -4,62 +4,38 @@ const api = axios.create({
   baseURL: "https://nc-news-cm.herokuapp.com/api",
 });
 
-const fetchArticles = (topic, sort, order) => {
-  let query = "/articles";
-  if (sort && !order) query += `?sort_by=${sort}`;
-  if (order && !sort) query += `?order=${order}`;
-  if (order && sort) query += `?sort_by=${sort}&order=${order}`;
-
+export function fetchArticles(topic, sort_by, order) {
   return api
-    .get(query, {
-      params: { topic: topic },
+    .get("/articles", {
+      params: { topic, sort_by, order },
     })
-    .then((res) => {
-      return res.data;
-    })
-    .catch((err) => console.log(err));
-};
+    .then((res) => res.data);
+}
 
-const fetchTopics = () => {
-  return api.get("/topics").then((res) => {
-    return res.data;
-  });
-};
+export function fetchTopics() {
+  return api.get("/topics").then((res) => res.data);
+}
 
-const fetchArticlesById = (id) => {
-  return api.get(`/articles/${id}`).then((res) => {
-    return res.data;
-  });
-};
+export function fetchArticlesById(id) {
+  return api.get(`/articles/${id}`).then((res) => res.data);
+}
 
-const amendVoteCount = (id, num) => {
-  return api.patch(`/articles/${id}`, { inc_votes: num }).then((res) => {
-    return res.data;
-  });
-};
+export function amendVoteCount(id, num) {
+  return api
+    .patch(`/articles/${id}`, { inc_votes: num })
+    .then((res) => res.data);
+}
 
-const fetchComments = (id) => {
-  return api.get(`/articles/${id}/comments`).then((res) => {
-    return res.data;
-  });
-};
+export function fetchComments(id) {
+  return api.get(`/articles/${id}/comments`).then((res) => res.data);
+}
 
-const fetchUsers = () => {
+export function fetchUsers() {
   return api.get("/users").then((res) => res.data);
-};
+}
 
-const postComment = (newComment, id) => {
+export function postComment(newComment, id) {
   return api
     .post(`/articles/${id}/comments`, newComment)
     .then((res) => res.data);
-};
-
-export {
-  fetchArticles,
-  fetchTopics,
-  fetchArticlesById,
-  amendVoteCount,
-  fetchComments,
-  fetchUsers,
-  postComment,
-};
+}
