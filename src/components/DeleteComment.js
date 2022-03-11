4a0b8d80b6b93899bb013f6f8676fb2cfcc49808
Clare30 <30181918+Clare30@users.noreply.github.com>
2022-03-11 15:38:48 +0000

    complete delete comment component

--- src/components/DeleteComment.js
+++ src/components/DeleteComment.js
@@ -4,16 +4,32 @@ import * as api from "../api";
 
 export default function DeleteComment({ comment, setComments }) {
   const [disabled, setDisable] = useState(false);
-
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
   const { loggedIn } = useContext(loggedInUser);
 
   const handleClick = () => {
+    setIsLoading(true);
     setDisable((currCount) => currCount + 1);
-    api.deleteComment(comment.comment_id).then(() => {
-      alert("comment deleted!");
-    });
+    api
+      .deleteComment(comment.comment_id)
+      .then(() => {
+        setComments((currComments) => {
+          return currComments.filter((currComment) => {
+            return currComment !== comment;
+          });
+        });
+        alert("Comment deleted!");
+        setIsLoading(false);
+        setDisable(0);
+      })
+      .catch(() => {
+        setError("Sorry, that didn't work, please try again");
+        setDisable(0);
+      });
   };
-
+  if (isLoading) return <p>Removing comment...</p>;
+  if (error) return alert(error);
   if (loggedIn === comment.author)
     return (
       <div>
@@ -23,5 +39,5 @@ export default function DeleteComment({ comment, setComments }) {
       </div>
     );
 
-  return <div></div>;
+  return <p></p>
 }
